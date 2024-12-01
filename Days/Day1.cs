1645e0c27e3f4351c7fc292ef0fe812b4711fbce
Daniel Lichtenstern <daniel@tomli.de>
2024-12-01 14:35:55 +0100

    Add project files.

---
+++ Days/Day1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC24.Days
+{
+    public static class Day1
+    {
+        public static string Ch1(string input)
+        {
+            List<int> left = new();
+            List<int> right = new();
+
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                var split = line.Split("   ");
+                left.Add(int.Parse(split[0]));
+                right.Add(int.Parse(split[1]));
+            }
+
+            left = left.Order().ToList();
+            right = right.Order().ToList();
+
+            int totalDiff = 0;
+            for (int i = 0; i < left.Count; i++)
+            {
+                totalDiff += Math.Abs(left[i] - right[i]);
+            }
+
+            return totalDiff.ToString();
+        }
+
+        public static string Ch2(string input)
+        {
+            List<int> left = new();
+            List<int> right = new();
+
+            foreach (var line in input.Split(Environment.NewLine))
+            {
+                var split = line.Split("   ");
+                left.Add(int.Parse(split[0]));
+                right.Add(int.Parse(split[1]));
+            }
+
+            int similarityScore = 0;
+            for (int i = 0; i < left.Count; i++)
+            {
+                var left_i = left[i];
+                similarityScore += right.Count(x => x == left_i) * left_i;
+            }
+
+            return similarityScore.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using AoC24.Days;
+
+string inp = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "input.txt"));
+
+Console.WriteLine(Day1.Ch2(inp));
+Console.ReadLine();
